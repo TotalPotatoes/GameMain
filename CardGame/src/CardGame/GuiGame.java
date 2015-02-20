aee0603e273d6a5efda5f643979fb837f813eab0
sazebrack <sazebrack@gmail.com>
2015-02-20 05:04:36 -0500

    Added information to Gui
    Name, HP, portrait, int, atk, def. ect

--- CardGame/src/CardGame/GuiGame.java
+++ CardGame/src/CardGame/GuiGame.java
@@ -1,6 +1,14 @@
 package CardGame;
+
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import java.awt.CardLayout;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import java.awt.Canvas;
+import java.awt.SystemColor;
+import java.awt.Color;
 
 /**
  * Gui Maybe?
@@ -42,8 +50,39 @@ public class GuiGame {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(0, 0, 500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setBounds(10, 11, 115, 14);
+		frame.getContentPane().add(lblName);
+		
+		JLabel lblStats = new JLabel("Stats");
+		lblStats.setBounds(10, 36, 37, 14);
+		frame.getContentPane().add(lblStats);
+		
+		JLabel labelHP = new JLabel("Hp:    100/100");
+		labelHP.setBounds(10, 56, 75, 14);
+		frame.getContentPane().add(labelHP);
+		
+		JLabel labelAttack = new JLabel("Attack      6");
+		labelAttack.setBounds(10, 76, 75, 14);
+		frame.getContentPane().add(labelAttack);
 
+		JLabel labelDefense = new JLabel("Defense    3");
+		labelDefense.setBounds(10, 96, 75, 14);
+		frame.getContentPane().add(labelDefense);
+		
+		JLabel labelSmarts = new JLabel("intellect    0");
+		labelSmarts.setBounds(10,116, 75, 14);
+		frame.getContentPane().add(labelSmarts);
+		
+		Canvas Picture = new Canvas();
+		Picture.setBackground(new Color(0, 0, 0));
+		Picture.setBounds(107, 31, 100, 100);
+		frame.getContentPane().add(Picture);
+		
+
+	}
 }
